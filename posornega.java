16518fdc6b5b9d40ea9b2db920c4e4b75f2bacf5
Deepakkzde <95161019+Deepakkzde@users.noreply.github.com>
2021-12-04 11:20:02 +0530

    Add files via upload

---
+++ posornega.java
@@ -0,0 +1,21 @@
+package Week1day2;
+
+public class posornega {
+
+	public static void main(String[] args) {
+		
+				int number =0;
+				if (number<0) {
+					System.out.println("The number is Negative");
+				}else if (number==0) {
+				System.out.println("The number is neither positive nor negative");
+					
+				}else {
+					System.out.println("The number is postive");
+				}
+				
+			}
+
+	
+
+}
